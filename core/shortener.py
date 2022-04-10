98dbd6a7efb9e5a38d570abe046eea774853ccc1
ghasem <ghasemgholi@hotmail.com>
2022-04-10 16:38:37 +0200

    shortener added

--- core/shortener.py
+++ core/shortener.py
@@ -1,18 +1,18 @@
 import hashlib
 
 class Shortener:
-    __URL_LEN = 5
-    __shortenedUrl = ""
+    URL_LEN = 5
+    shortenedUrl = ""
     def __init__(self, url):
         self.__shortenUrl(url)
     
     def __shortenUrl(self, url):
         hashedUrl = hashlib.sha512(url.encode())
         hexifiedUrl = hashedUrl.hexdigest()
-        self.shortenedUrl = hexifiedUrl[0:self.__URL_LEN]
+        self.shortenedUrl = hexifiedUrl[0:self.URL_LEN]
 
     def _print(self):
-        print(self.__shortenedUrl)
+        print(self.shortenedUrl)
 
 if __name__ == "__main__":
     sh = Shortener("https://stackoverflow.com/questions/1641219/does-python-have-private-variables-in-classes")
