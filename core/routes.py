d60be483eae059363bdf7a011389a44261e7a7a1
willemebe.vd.spek@gmail.com <willemebe.vd.spek@gmail.com>
2022-04-10 16:18:48 +0200

    added gitignore and fixed bug

--- core/routes.py
+++ core/routes.py
@@ -2,40 +2,14 @@ from flask import *
 from core import app, db
 from core.models import Urls
 
-app = Flask(__name__)
-app.config['SQL_ALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQL_ALCHEMY_DATABSE_URI'] = 'sqlite:///urls.db'
 
-
-@app.before_first_request
-def init_tables():
-    '''
-    Initialize the table containing the urls.
-    '''
-    db.create_all()
-
-
-@app.get('/')
+@app.route('/', methods=['GET', 'POST', 'DELETE'])
 def index():
     '''
     Set up the route to the index page. 
     '''
-    # Handle incoming POST request
-    if request.method == 'POST':
-        url = request.form['url']
-
-        if not is_url(url):
-            flash('Please enter a valid URL')
-            return redirect(url_for('index'))
-
-        return render_template('./index.html')
-
-    elif request.method == 'GET':
-        return render_template('./index.html')
+    return render_template('index.html')
     
-    else: 
-        return bad_request('This request method is invalid.')
-
 
 def is_url(url):
     '''
