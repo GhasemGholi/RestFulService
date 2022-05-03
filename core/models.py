669ff92d4de4843f94a86a95a7af070e9e8bbb43
willemebe.vd.spek@gmail.com <willemebe.vd.spek@gmail.com>
2022-05-03 13:08:37 +0200

    fixed bug, added docker-compose,yml

--- core/models.py
+++ core/models.py
@@ -16,8 +16,8 @@ class Users(db_api.Model):
     TODO: Implement a database model for the users. 
           Should contain id, username and hashed password at least. 
     '''
-    id = db_api.Column("id", db_api.Binary(), primary_key=True)
-    key = db_api.Column("key", db_api.String())
+    id = db_api.Column("id", db_api.Integer(), primary_key=True)
+    key = db_api.Column("key", db_api.LargeBinary())
     user = db_api.Column("user", db_api.String())
     password = db_api.Column("password", db_api.String())
     token = ""
@@ -37,11 +37,11 @@ class Users(db_api.Model):
         self.user = user
         self.password = password
         self.key = self.tokenize()
-        self.id = self.createID()
+        # self.id = self.createID()
     
     def checkToken(self):
         check = jwt.decode(self.key, "secret", algorithms=["HS256"])
-        if check is not self.key:
+        if check != {self.user : self.password}:
             return False
         return True
     
@@ -57,8 +57,8 @@ class Users(db_api.Model):
     def getUser(self):
         return self.user
     
-    def createID(self):
-        return secrets.token_bytes(nbytes=8)
+    # def createID(self):
+    #     return secrets.token_bytes(nbytes=8)
     
     def getID(self):
         return self.id
