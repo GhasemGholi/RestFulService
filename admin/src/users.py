9f91a4738aa8a9380464ca6718d9b750e64bfa7b
ghasem <ghasemgholi@hotmail.com>
2022-05-09 04:12:11 +0200

    docker is working

--- admin/src/users.py
+++ admin/src/users.py
@@ -61,7 +61,8 @@ def login():
     currentUser = user
     isLoggedIn = True
     
-    r = requests.get('http://127.0.0.1:5000/newlogin/'+user) 
+    r = requests.get('http://172.17.0.2:5000/newlogin/'+user) 
+    # r = requests.get('http://127.0.0.1:5000/newlogin/'+user) 
     
     return make_response("LOGGED IN", 200)
 
